9834cbdf287668c6d140c239e8c9c2e4c461278c
Dev Chrysalis Dalal <dev@deviaze.com>
2025-01-18 20:08:59 -0600

    fix: major fixes to tracebacks to show accurate luau file names, custom parsing of mlua traceback stuff, etc.

---
+++ src/error_handling.rs
@@ -0,0 +1,36 @@
+#![allow(dead_code)]
+
+use mlua::prelude::*;
+use crate::globals;
+
+type LuaValueResult = LuaResult<LuaValue>;
+
+pub fn parse_traceback(raw_traceback: String) -> String {
+    let parse_traceback = include_str!("./parse_traceback.luau");
+    let luau_for_traceback = Lua::new();
+    globals::set_globals(&luau_for_traceback).unwrap();
+    match luau_for_traceback.load(parse_traceback).eval() {
+        Ok(LuaValue::Function(parse_traceback)) => {
+            parse_traceback.call::<LuaString>(raw_traceback)
+                .unwrap()
+                .to_string_lossy()
+                .to_string()
+        },
+        Ok(other) => {
+            panic!("parse_traceback.luau should return a function??, got: {:#?}", other);
+        },
+        Err(err) => {
+            panic!("parse_traceback.luau broke with err: {err}");
+        },
+    }
+}
+
+#[macro_export]
+macro_rules! wrap_err {
+    ($msg:expr) => {
+        Err(LuaError::external(format!("{}{}{}", colors::RED, $msg, colors::RESET)))
+    };
+    ($msg:expr, $($arg:tt)*) => {
+        Err(LuaError::external(format!("{}{}{}", colors::RED, format!($msg, $($arg)*), colors::RESET)))
+    };
+}
